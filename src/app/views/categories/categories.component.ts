73d102c0a8e487f399ac3be4c854b8b7b730ff93
Andrii Kachkarov <obsessedbyu@gmail.com>
2020-06-16 14:24:58 +0300

    Add dynamic output of categories in template

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DataHandlerService} from '../../services/data-handler.service';
+import {Category} from '../../model/Category';
 
 @Component({
   selector: 'app-categories',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent implements OnInit {
+  categories: Category[];
 
-  constructor() { }
+  constructor(private dataHandler: DataHandlerService) { }
 
   ngOnInit(): void {
+    this.categories = this.dataHandler.getCategories();
   }
 
 }
